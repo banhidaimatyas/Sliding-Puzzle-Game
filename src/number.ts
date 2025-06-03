d609cdf4752a15900ecea0655fba54c8ca7e35fe
banhidaimatyas <banhidai.matyas@students.jedlik.eu>
2025-06-03 18:59:39 +0200

    setPosition createElement nélkül

--- src/number.ts
+++ src/number.ts
@@ -1,20 +1,38 @@
 export class NumberTile {
-  value: number;
-  numbers: NumberTile[];
-  x: number | undefined;
-  y: number | undefined;
-  size: number;
+    value: number;
+    numbers: NumberTile[];
+    x: number | undefined;
+    y: number | undefined;
+    size: number;
 
-  constructor(value: number, numbers: NumberTile[], size: number) {
-    if (value < 1 || value >= size * size) {
-      throw new Error(`A value értékének 1 és ${size * size - 1} között kell lennie.`);
+    constructor(value: number, numbers: NumberTile[], size: number) {
+        if (value < 1 || value >= size * size) {
+            throw new Error(`A value értékének 1 és ${size * size - 1} között kell lennie.`);
+        }
+
+        this.value = value;
+        this.numbers = numbers;
+        this.size = size;
+        this.x = undefined;
+        this.y = undefined;
+
     }
 
-    this.value = value;
-    this.numbers = numbers;
-    this.size = size;
-    this.x = undefined;
-    this.y = undefined;
+    setPosition(x: number, y: number): void {
+        // Ha a számnak már volt korábbi pozíciója, kitörli onnan
+        if (this.x !== undefined && this.y !== undefined) {
+            const oldId = `cell-${this.x}-${this.y}`;
+            const oldCell = document.getElementById(oldId);
+            if (oldCell) oldCell.innerHTML = '';
+        }
 
-  }
+        // Létrehoz egy új div elemet a számhoz
+        // const div = this.createElement();
+        const newId = `cell-${x}-${y}`;
+        const newCell = document.getElementById(newId);
+        // if (newCell) newCell.appendChild(div);
+
+        this.x = x;
+        this.y = y;
+    }
 }
