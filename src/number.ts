449855edd0f88309df74dd26243544c2dc1b60b8
banhidaimatyas <banhidai.matyas@students.jedlik.eu>
2025-06-03 19:31:56 +0200

    kommentek kivétele

--- src/number.ts
+++ src/number.ts
@@ -19,14 +19,12 @@ export class NumberTile {
     }
 
     public setPosition(x: number, y: number): void {
-        // Ha a számnak már volt korábbi pozíciója, kitörli onnan
         if (this.x !== undefined && this.y !== undefined) {
             const oldId = `cell-${this.x}-${this.y}`;
             const oldCell = document.getElementById(oldId);
             if (oldCell) oldCell.innerHTML = '';
         }
 
-        // Létrehoz egy új div elemet a számhoz
         const div = this.createElement();
         const newId = `cell-${x}-${y}`;
         const newCell = document.getElementById(newId);
@@ -37,12 +35,10 @@ export class NumberTile {
     }
 
     private createElement(): HTMLDivElement {
-        const div = document.createElement('div'); // 1. Létrehoz egy új div elemet a számhoz
-        // 2. Beállítja a megfelelő osztályt és szövegét
+        const div = document.createElement('div');
         div.className = 'number';
         div.dataset.number = this.value.toString();
         div.innerText = this.value.toString();
-        // 3. Hozzáad egy kattintás eseménykezelőt
         div.addEventListener('click', () => {
             const emptyPosition = this.emptyPosition;
             if (emptyPosition !== null) {
