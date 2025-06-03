38d7142bf11911675d39e50fd5c302dc3e706454
majorbalintistvan <major.balint.istvan@students.jedlik.eu>
2025-06-03 19:27:36 +0200

    nem jó, de legalább valami

--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,17 @@
-console.log()
+import { NumberTile } from './number.ts';
+
+const size: number = 10;
+const numbers: NumberTile[] = [];
+//setupgrid(size);
+//placenumbers();
+
+function isfree(x: number, y: number): boolean {
+    const index = y * size + x;
+    const cell = document.querySelector(`.grid-item:nth-child(${index + 1})`);
+    return cell !== null && cell.textContent === '';
+}
+
+
+function randomcord():number{
+    return Math.floor(Math.random()*size)
+}
